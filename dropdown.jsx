b4212cd3579c54115f4f01323df3347b8e97b044
devcrowley <thedeadtaco@gmail.com>
2022-03-08 13:11:57 -0800

    Beta Deploy

---
+++ dropdown.jsx
@@ -0,0 +1,144 @@
+/*
+
+dropdown.jsx
+
+A filterable dropdown selector
+
+Passable Properties:
+
+value:      The starting / default value
+onChange:   Function to run when the value changes (returns value as the only argument)
+expanded    Always show the list of choices as expanded when focused
+styleSheet  A stylesheet to override the main stylesheet
+
+Copyright 2022, Devin Crowley
+
+Developer NOTE:  Similar functionality already exists natively in HTML5, but the dropdown.jsx
+version has some fancy stylings and handlers.  Also, the native dropdown shows values AND value
+text together which can't be changed through simple css or attributes
+
+    <input type="text" name="product" list="productName"/>
+    <datalist id="productName">
+        <option value="Pen">Pen</option>
+        <option value="Pencil">Pencil</option>
+        <option value="Paper">Paper</option>
+    </datalist>
+
+*/
+
+import { cloneElement, options } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import style from './dropdown.css';
+
+function Dropdown( {children, value, onChange, expanded, styleSheet = {}} ) {
+
+    // Append any custom stylesheets passed via the styleSheet prop
+    styleSheet = {...style, ...styleSheet};
+
+    // Give access to all states and setStates to child elements
+    const state = {};
+    state.onChange = onChange;
+    state.alwaysExpanded = expanded;
+    [state.expanded, state.setExpanded] = useState(false);
+    [state.filter, state.setFilter] = useState("");
+    [state.value, state.setValue] = useState("");
+
+    // Access to DOM elements to change stylings after rendering as needed
+    const domElement = {};
+
+    // Sets the width of the dropdown to match the full inout area
+    useEffect(()=>{
+        domElement.options.style.width = domElement.input.getBoundingClientRect().width - 1 + "px";
+    })
+
+    return (
+        <div className={styleSheet.dropdown}>
+            <div className={styleSheet.input}>
+
+                {/* The filter/search input for filtering the options in the list */}
+                <input 
+                onBlur={()=>{
+                    if(state.expanded) {
+                        setTimeout(()=>state.setExpanded(false), 10);
+                    }
+                }}
+                onInput={handleInputChange}
+                onFocus={handleInputChange}
+                value={state.filter}
+                ref={elem=>{
+                    domElement.input = elem;
+                }}
+                />
+
+                {/* A button to clear the input */}
+                <div className={styleSheet.btn_clear}
+                onClick={()=>{
+                    if(state.filter !== "") state.setFilter("");
+                    if(state.value !== "") {;
+                        state.setValue("");
+                        if(state.onChange) state.onChange("");
+                    }
+                }}
+                ></div>
+
+                {/* A button for expanding the options list */}
+                <button className={styleSheet.btn_expand}
+                onClick={()=>{
+                    state.setExpanded(!state.expanded);
+                }}
+                ></button>
+
+            </div>
+
+            <div 
+            className={state.expanded ? styleSheet.expanded : styleSheet.collapsed}
+            ref={elem=>{
+                domElement.options = elem;
+            }}
+            >
+                <FilteredChildren state={state}>{children}</FilteredChildren>
+            </div>
+            
+        </div>
+    )
+
+    function handleInputChange(evt) {
+        if(evt.target.value !== "" || state.alwaysExpanded) {
+            state.setExpanded(true);
+        } else {
+            state.setExpanded(false);
+        }
+        state.setExpanded(true);
+        state.setFilter(evt.target.value);
+    }
+}
+
+/**
+ * Filters the list of options based on their text content
+ * @param {*} param0 
+ * @returns 
+ */
+function FilteredChildren({children, state}) {
+    const options = [];
+
+    // Add events to all child elements (options) to handle clicks
+    children.forEach(c=>{
+        if(c.props.children.join('').toLowerCase().trim().includes(state.filter.toLowerCase().trim())) {
+            const childClone = cloneElement(c, { onMouseDown: handleClick });
+            options.push(childClone);
+        }
+    })
+
+    // Click handler for all child elements (options)
+    function handleClick(evt) {
+        state.setValue(evt.target.value);
+        state.setFilter(evt.target.innerText);
+        state.setExpanded(false);
+        if(state.onChange) state.onChange(evt.target.value);
+    }
+
+    return options;
+}
+
+export default Dropdown;
+
